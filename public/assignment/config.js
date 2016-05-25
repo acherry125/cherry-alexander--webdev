66ca7e36418408470fd2ce0a47f4dca8166b9a5b
Alex Cherry <Alex Cherry>
2016-05-25 17:16:35 -0400

    linked page controllers

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -47,13 +47,19 @@
                 controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page", {
-            	templateUrl: "views/page/page-list.view.client.html"
+            	templateUrl: "views/page/page-list.view.client.html",
+                controller: "PageListController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/new", {
-            	templateUrl: "views/page/page-new.view.client.html"
+            	templateUrl: "views/page/page-new.view.client.html",
+                controller: "PageNewController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/:pid", {
-            	templateUrl: "views/page/page-edit.view.client.html"
+            	templateUrl: "views/page/page-edit.view.client.html",
+                controller: "PageEditController",
+                controllerAs: "model"
             })
             .when("/user/:uid/website/:wid/page/:pid/widget", {
                 templateUrl: "views/widget/widget-list.view.client.html"
