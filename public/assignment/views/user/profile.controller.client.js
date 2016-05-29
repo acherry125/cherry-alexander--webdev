230c311a06d6bf7eccaa7145485a7cb7a9e0f0ae
Alex Cherry <Alex Cherry>
2016-05-29 13:33:13 -0400

    removed website home button

--- public/assignment/views/user/profile.controller.client.js
+++ public/assignment/views/user/profile.controller.client.js
@@ -12,8 +12,8 @@
         vm.updateUser = updateUser;
         vm.navigateToProfile = navigateToProfile;
         
-        var id = $routeParams["uid"];
-        vm.uId = id;
+        var uid = $routeParams["uid"];
+        vm.uId = uid;
 
         function init() {
             vm.user = angular.copy(UserService.findUserById(id));
@@ -23,15 +23,11 @@
         
         function updateUser() {
             if (vm.user) {
-                UserService.updateUser(id, vm.user);
+                UserService.updateUser(uid, vm.user);
                 vm.success = "User succesfully updated their profile";
             } else {
                 vm.failure = "Error: User does not exist, cannot update non-existent user";
             }
-        }
-
-        function navigateToProfile() {
-            $location.url("/user/" + id);
         }
     }
 })();
