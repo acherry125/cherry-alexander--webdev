64aa060ab19e11590258cdfca04176c5cdb88d26
Alex Cherry <Alex Cherry>
2016-06-04 14:29:33 -0400

    added local register validation

--- public/assignment/views/user/register.controller.client.js
+++ public/assignment/views/user/register.controller.client.js
@@ -20,41 +20,36 @@
 
         /* Registers a new user */
         function register(username, password, verify) {
-            newUser = { username: username, password: password, verify: verify};
-            UserService
-                .createUser(newUser)
-                .then(
-                    function(response) {
-                        $location.url("/user/" + response.data);
-                    },
-                    function(error) {
-                        vm.failure = error.data;
-                    }
-                )
+            if(verifyLocally(username, password, verify)) {
+                newUser = {username: username, password: password, verify: verify};
+                UserService
+                    .createUser(newUser)
+                    .then(
+                        function (response) {
+                            $location.url("/user/" + response.data);
+                        },
+                        function (error) {
+                            vm.failure = error.data;
+                        }
+                    )
+            }
             
-        }
+        };
 
         /* helper function to verify registration form */
-        function verifyRegistration(username, password, verify) {
-            UserService
-                .findUserByUsername(username)
-                .then(function(response) {
-                    // check username available (finduserbyusername)
-                    if (response.data._id) {
-                        vm.failure = "Username is already taken";
-                    } else if(!(username && password && verify)) {
-                        vm.failure = "All fields must be filled in"
-                    }
-                    // check passwords match
-                    else if (!(password === verify)) {
-                        vm.failure = "Password and Verify Password fields must match";
-                    }
-                    // success
-                    else {
-                        return true;
-                    }
-                });
-        }
+        function verifyLocally(username, password, verify) {
+            if(!(username && password && verify)) {
+                vm.failure = "All fields must be filled in"
+            }
+            // check passwords match
+            else if (!(password === verify)) {
+                vm.failure = "Password and Verify Password fields must match";
+            }
+            // success
+            else {
+                return true;
+            }
+        };
 
 
     }
