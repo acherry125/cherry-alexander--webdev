c0fa9f1c0148408f7c0a0361d2e4b16ed37821d4
Alex Cherry <Alex Cherry>
2016-06-04 17:38:42 -0400

    removed comments from app.js

--- assignment/app.js
+++ assignment/app.js
@@ -6,21 +6,7 @@ module.exports = function(app) {
 	var websiteService = require("./services/website.service.server.js")(app);
 	var pageService = require("./services/page.service.server.js")(app);
 	var widgetService = require("./services/widget.service.server.js")(app);
+	var flickrService = require("./services/flickr.service.server.js")(app);
 
 
-	// listen for incoming get request (using express.js) to this path
-	// execute the function when heard
-	// "req" is from express to grab the "message" variable
-	// FIRST ARG IS ALWAYS REQUEST, CAN NAME IT ANYTHING
-	// SECOND ARG IS ALWAYS RESPONSE, CAN NAME IT ANYTHING
-	/*app.get("/say/:message", function(req) {
-		var msg = req.params["message"];
-		console.log(msg);
-	});
-	/* Send it to the browser
-	app.get("/say/:message", function(req, res) {
-		var msg = req.params["message"];
-		res.send("This is the the message " + msg);
-	})
-	*/
 };
