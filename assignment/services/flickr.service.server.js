29384725875b996027693e44d6c7d2ea566be9ff
Alex Cherry <Alex Cherry>
2016-06-05 12:21:54 -0400

    tweaked vertical spacing in flickr grid

--- assignment/services/flickr.service.server.js
+++ assignment/services/flickr.service.server.js
@@ -7,11 +7,13 @@ module.exports = function(app, requester) {
     key = "aa48e87fb459d9d7dfc3391515792913";
 
     var urlBase = "https://api.flickr.com/services/rest/?method=flickr.photos.search" +
-        "&format=json&api_key=API_KEY&text=TEXT&sort=relevance&media=photos";
-    
+        "&format=json&api_key=API_KEY&text=TEXT&sort=relevance&media=photos&page=PAGE" +
+        "&per_page=105&page";
+
     function makeRequest(req, res) {
         term = req.query.term;
-        url = urlBase.replace("TEXT", term).replace("API_KEY", key);
+        page = req.query.page;
+        url = urlBase.replace("TEXT", term).replace("API_KEY", key).replace("PAGE", page);
         console.log(url);
 
         requester(url, function (error, response, body) {
