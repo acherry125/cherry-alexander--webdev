ea326511dbc909018685e22f203d9a12947c97a2
Alex Cherry <Alex Cherry>
2016-06-08 01:16:32 -0400

    still trying to fix openshift

--- assignment/models/models.js
+++ assignment/models/models.js
@@ -1,7 +1,8 @@
 module.exports = function() {
 
     var mongoose = require('mongoose');
-   // mongoose.connect('mongodb://localhost/webdev');
+   // mongoose.connect('mongodb://localhost/test');
+   mongoose.connect('mongodb://127.0.0.1:27017/test');
 
     var userModel = require("./user/user.model.server.js")(mongoose);
     var websiteModel = require("./website/website.model.server.js")(mongoose);
