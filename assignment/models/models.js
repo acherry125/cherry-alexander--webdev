1a9fe490ada53ab93bcc899f9e9d1a9924e706ff
Alex Cherry <Alex Cherry>
2016-06-08 01:20:28 -0400

    still trying to fix openshift... still

--- assignment/models/models.js
+++ assignment/models/models.js
@@ -1,8 +1,28 @@
 module.exports = function() {
 
     var mongoose = require('mongoose');
-   // mongoose.connect('mongodb://localhost/test');
-   mongoose.connect('mongodb://127.0.0.1:27017/test');
+    //mongoose.connect('mongodb://localhost/test');
+    var url = '127.0.0.1:27017/' + process.env.OPENSHIFT_APP_NAME;
+
+	// if OPENSHIFT env variables are present, use the available connection info:
+	if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+    	url = process.env.OPENSHIFT_MONGODB_DB_URL +
+    	process.env.OPENSHIFT_APP_NAME;
+	}
+
+	// Connect to mongodb
+	var connect = function () {
+    	mongoose.connect(url);
+	};
+	connect();
+
+	var db = mongoose.connection;
+
+	db.on('error', function(error){
+    	console.log("Error loading the db - "+ error);
+	});
+
+	db.on('disconnected', connect);
 
     var userModel = require("./user/user.model.server.js")(mongoose);
     var websiteModel = require("./website/website.model.server.js")(mongoose);
