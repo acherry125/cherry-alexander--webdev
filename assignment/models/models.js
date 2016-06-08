a0ddb71580a472e8f81003e2cf0dc130c40e3376
Alex Cherry <Alex Cherry>
2016-06-08 13:46:00 -0400

    changed connection to mongodb

--- assignment/models/models.js
+++ assignment/models/models.js
@@ -4,28 +4,19 @@ module.exports = function() {
 
     /* found here, makes it work with OpenShift */
     // http://stackoverflow.com/questions/29073657/mongoose-openshift-connection
-    var url = '127.0.0.1:27017/webdev';
 
-	// if OPENSHIFT env variables are present, use the available connection info:
-	if (process.env.OPENSHIFT_MONGODB_DB_URL) {
-    	url = process.env.OPENSHIFT_MONGODB_DB_URL +
-    	process.env.OPENSHIFT_APP_NAME;
-	}
 
-	// Connect to mongodb
-	var connect = function () {
-    	mongoose.connect(url);
-	};
-	connect();
+    var connectionString = 'mongodb://127.0.0.1:27017/webdev';
 
-	var db = mongoose.connection;
+    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+            process.env.OPENSHIFT_APP_NAME;
+    }
+    mongoose.connect(connectionString);
 
-	db.on('error', function(error){
-    	console.log("Error loading the db - "+ error);
-	});
-
-	db.on('disconnected', connect);
-	/* end of found on the internet portion */
 
     var userModel = require("./user/user.model.server.js")(mongoose);
     var websiteModel = require("./website/website.model.server.js")(mongoose);
