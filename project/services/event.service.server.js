de11d6d97243337c863be05e78bccbc674fd1ce0
Alex Cherry <Alex Cherry>
2016-06-20 16:12:47 -0400

    added function structure for event server service, changed create POST uri for organizations

--- project/services/event.service.server.js
+++ project/services/event.service.server.js
@@ -2,5 +2,40 @@ module.exports = function(app, models) {
 
     var eventModel = models.eventModel;
 
+    // create an event for an organization
+    app.post("/api/project/organization/:oid/event", createEvent);
+    // update an event
+    app.put("/api/project/event/:eid", updateEvent);
+    // remove an event
+    app.delete("/api/project/event/:eid", removeEvent);
+    // find an event by id
+    app.get("/api/project/event/:eid", findEventById);
+    // find all events for an organization
+    app.get("/api/project/organization/:oid/event", findEventForOrganization);
+
+    // create an organization for a user
+    function createEvent(req, res) {
+        res.sendStatus(200);
+    }
+
+    // update an organization
+    function updateEvent(req, res) {
+        res.sendStatus(200);
+    }
+
+    // remove an organization
+    function removeEvent(req, res) {
+        res.sendStatus(200);
+    }
+
+    // find an organization by id
+    function findEventById(req,res) {
+        res.sendStatus(200);
+    }
+
+    // find all organizations for a poster
+    function findEventForOrganization(req, res) {
+        res.sendStatus(200);
+    }
 
 };
--- project/services/organization.service.server.js
+++ project/services/organization.service.server.js
@@ -3,32 +3,37 @@ module.exports = function(app, models) {
     var organizationModel = models.organizationModel;
 
     // create an organization for a user
-    app.post("/api/project/organization", createOrganization);
+    app.post("/api/project/user/:uid/organization", createOrganization);
     // update an organization
     app.put("/api/project/organization/:oid", updateOrganization);
     // remove an organization
-    app.delete("/api/project/organization", removeOrganization);
+    app.delete("/api/project/organization/:oid", removeOrganization);
     // find an organization by id
     app.get("/api/project/organization/:oid", findOrganizationById);
     // find all organizations for a poster
     app.get("/api/project/user/:uid/organization", findOrganizationForPoster);
 
+    // create an organization for a user
     function createOrganization(req, res) {
         res.sendStatus(200);
     }
 
+    // update an organization
     function updateOrganization(req, res) {
         res.sendStatus(200);
     }
 
+    // remove an organization
     function removeOrganization(req, res) {
         res.sendStatus(200);
     }
 
+    // find an organization by id
     function findOrganizationById(req,res) {
         res.sendStatus(200);
     }
 
+    // find all organizations for a poster
     function findOrganizationForPoster(req, res) {
         res.sendStatus(200);
     }
