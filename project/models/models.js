7185121d0fb05bc984fdf79a638c626ab2416047
Alex Cherry <Alex Cherry>
2016-06-20 00:56:28 -0400

    fixed error, added client services to index

--- project/models/models.js
+++ project/models/models.js
@@ -3,8 +3,8 @@ module.exports = function() {
     var mongoose = require('mongoose');
     
     var userModel = require("./user/user.model.server.js")(mongoose);
-    var organizationModel = require("./user/organization.model.server.js")(mongoose);
-    var eventModel = require("./user/event.model.server.js")(mongoose);
+    var organizationModel = require("./organization/organization.model.server.js")(mongoose);
+    var eventModel = require("./event/event.model.server.js")(mongoose);
 
 
     var models = {
