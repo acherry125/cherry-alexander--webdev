85a768a39f1c7a9be332a544f381198e1fd2b2d9
Alex Cherry <Alex Cherry>
2016-06-22 14:44:16 -0400

    starting reply (delete message still not done)

--- public/project/views/user/messages.controller.client.js
+++ public/project/views/user/messages.controller.client.js
@@ -36,6 +36,22 @@
             vm.messageDict[messageId] = !vm.messageDict[messageId]; 
         }
 
+        function replyMessage(recipientId, orgName) {
+            // vm.messageContent doesn't seme like a good solution
+            var message = {name: vm.user.username, organization: orgName, message: vm.messageContent};
+            UserService
+                .sendMessage(recipientId, userId, message)
+                .then(
+                    function(response) {
+                        vm.messageContent = "";
+                        vm.success = "Message Sent!"
+                    },
+                    function(error) {
+                        vm.error = error.data
+                    }
+                )
+        }
+
     }
 
 })();
