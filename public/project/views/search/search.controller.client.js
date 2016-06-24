110f618c7fd6471c6850846a58caa160bb242f90
Alex Cherry <Alex Cherry>
2016-06-24 19:16:42 -0400

    search map will update markers that are both valid (from the future) and within bounds and will show events on map below

--- public/project/views/search/search.controller.client.js
+++ public/project/views/search/search.controller.client.js
@@ -4,12 +4,13 @@
         .module("EventHorizon")
         .controller("SearchController", SearchController);
 
-    function SearchController($rootScope, EventService, UserService, $location) {
+    function SearchController($rootScope, EventService, UserService, $scope, $location) {
         var vm = this;
         vm.searchTerm = "";
         vm.mapDisplay = false;
         vm.goToLogin = goToLogin;
         vm.markers = [];
+        vm.visibleEvents = [2];
         // just for rendering, doesn't allow access to anything sensitive
         vm.user = $rootScope.currentUser;
 
@@ -32,51 +33,76 @@
         function addMarkers(events, map) {
             for (var i = 0; i < events.length; i++) {
                 var event = events[i];
-                var location = event.location;
-                var placeId = location.place_id;
-                var LatLng = new google.maps.LatLng(location.lat, location.lng);
-                var marker = new google.maps.Marker({
-                    title: event.name,
-                    map: map
-                });
-                marker.setPlace({
-                    location: LatLng,
-                    placeId: placeId
-                });
+                var then = new Date(event.date);
+                var now = new Date();
+                var yearCheck = then.getYear() > now.getYear();
+                var monthCheck = then.getYear() === now.getYear()
+                    && then.getMonth() > now.getMonth();
+                var dayCheck = then.getYear() === now.getYear()
+                    && then.getMonth() === now.getMonth()
+                    && then.getDate() >= now.getDate();
+                // marker for an event yet to come
+                if (yearCheck || monthCheck || dayCheck) {
+                    var location = event.location;
+                    var placeId = location.place_id;
+                    var LatLng = new google.maps.LatLng(location.lat, location.lng);
+                    var marker = new google.maps.Marker({
+                        title: event.name,
+                        map: map
+                    });
+                    marker.setPlace({
+                        location: LatLng,
+                        placeId: placeId
+                    });
 
-                var phone = event.phone ? event.phone : "";
+                    var phone = event.phone ? event.phone : "";
 
 
-                marker.info = new google.maps.InfoWindow({
-                    content: '<div><strong>' + '<a href="#/event/' + event._id + '">' + event.name + '</a></strong><br>' +
-                    location.address + '<br>' + phone + '</div>'
-                });
+                    marker.info = new google.maps.InfoWindow({
+                        content: '<div><strong>' + '<a href="#/event/' + event._id + '">' + event.name + '</a></strong><br>' +
+                        location.address + '<br>' + phone + '</div>'
+                    });
 
-                google.maps.event.addListener(marker, 'click', function() {
-                    var marker_map = this.getMap();
-                    this.info.open(marker_map, this);
-                });
+                    google.maps.event.addListener(marker, 'click', function() {
+                        var marker_map = this.getMap();
+                        this.info.open(marker_map, this);
+                    });
 
-                vm.markers.push(marker);
+                    vm.markers.push(marker);
+                } else {
+                    // to keep number of events == number of markers in arrays
+                    vm.markers.push(0);
+                }
             }
         }
 
         function showVisibleMarkers(map) {
             var bounds = map.getBounds(),
                 count = 0;
-
-            for (var i = 0; i < markers.length; i++) {
-                var marker = markers[i];
-                if (bounds.contains(marker.getPosition())===true) {
-                    // dummy code
-                    x = 2;
-                    // marker.open()
+            vm.visibleEvents = [];
+            for (var i = 0; i < vm.markers.length; i++) {
+                var marker = vm.markers[i];
+                if(marker === 0) {
+                    continue;
+                }
+                var lat = marker.place.location.lat();
+                var lng = marker.place.location.lng();
+                var latLng = new google.maps.LatLng(lat, lng);
+                if (bounds.contains(latLng) === true) {
+                    vm.visibleEvents.push(vm.events[i]);
+                    marker.setMap(map);
                 } else {
-                    // dummy code
-                    x = 3;
-                    // marker.close()
+                    marker.setMap(null);
                 }
             }
+            vm.visibleEvents.sort(eventComparatorRevDate);
+            // outside of angular call so must apply
+            $scope.$apply();
+        }
+
+        // orders events by reverse date so they render from top to bottom
+        function eventComparatorRevDate(event1, event2) {
+            return event1.date > event2.date;
         }
 
         function map_init() {
