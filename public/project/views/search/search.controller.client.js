43f0dc9cc1796c73a2e715821bc3d616a2c4731e
Alex Cherry <Alex Cherry>
2016-06-24 19:28:45 -0400

    added custom marker! improved logic of map rendering

--- public/project/views/search/search.controller.client.js
+++ public/project/views/search/search.controller.client.js
@@ -30,6 +30,58 @@
 
         init();
 
+        function map_init() {
+            var mapOptions = {
+                center: {lat: 42.34003, lng: -71.089797},
+                zoom: 16
+            };
+            var map = new google.maps.Map(document.getElementById('map'),
+                mapOptions);
+
+            addMarkers(vm.events, map);
+
+            // Fired when the map becomes idle after panning or zooming.
+            google.maps.event.addListener(map, 'idle', function() {
+                showVisibleMarkers(map);
+            });
+
+            var input = /** @type {HTMLInputElement} */(
+                document.getElementById('search'));
+
+            // Create the autocomplete helper, and associate it with
+            // an HTML text input box.
+            var autocomplete = new google.maps.places.Autocomplete(input);
+            autocomplete.bindTo('bounds', map);
+
+            var marker = new google.maps.Marker({
+                map: map
+            });
+
+            // Get the full place details when the user selects a place from the
+            // list of suggestions.
+            google.maps.event.addListener(autocomplete, 'place_changed', function() {
+                var place = autocomplete.getPlace();
+                if (!place.geometry) {
+                    return;
+                }
+
+                if (place.geometry.viewport) {
+                    map.fitBounds(place.geometry.viewport);
+                } else {
+                    map.setCenter(place.geometry.location);
+                    map.setZoom(17);
+                }
+
+                // Set the position of the marker using the place ID and location.
+                marker.setPlace(/** @type {!google.maps.Place} */ ({
+                    placeId: place.place_id,
+                    location: place.geometry.location
+                }));
+                marker.setVisible(true);
+            });
+        }
+
+
         function addMarkers(events, map) {
             for (var i = 0; i < events.length; i++) {
                 var event = events[i];
@@ -48,7 +100,8 @@
                     var LatLng = new google.maps.LatLng(location.lat, location.lng);
                     var marker = new google.maps.Marker({
                         title: event.name,
-                        map: map
+                        map: map,
+                        icon: "/image/marker.png"
                     });
                     marker.setPlace({
                         location: LatLng,
@@ -105,56 +158,6 @@
             return event1.date > event2.date;
         }
 
-        function map_init() {
-            var mapOptions = {
-                center: {lat: 42.34003, lng: -71.089797},
-                zoom: 16
-            };
-            var map = new google.maps.Map(document.getElementById('map'),
-                mapOptions);
-
-            addMarkers(vm.events, map);
-
-            // Fired when the map becomes idle after panning or zooming.
-            google.maps.event.addListener(map, 'idle', function() {
-                showVisibleMarkers(map);
-            });
-
-            var input = /** @type {HTMLInputElement} */(
-                document.getElementById('search'));
-
-            // Create the autocomplete helper, and associate it with
-            // an HTML text input box.
-            var autocomplete = new google.maps.places.Autocomplete(input);
-            autocomplete.bindTo('bounds', map);
-
-            var marker = new google.maps.Marker({
-                map: map
-            });
-
-            // Get the full place details when the user selects a place from the
-            // list of suggestions.
-            google.maps.event.addListener(autocomplete, 'place_changed', function() {
-                var place = autocomplete.getPlace();
-                if (!place.geometry) {
-                    return;
-                }
-
-                if (place.geometry.viewport) {
-                    map.fitBounds(place.geometry.viewport);
-                } else {
-                    map.setCenter(place.geometry.location);
-                    map.setZoom(17);
-                }
-
-                // Set the position of the marker using the place ID and location.
-                marker.setPlace(/** @type {!google.maps.Place} */ ({
-                    placeId: place.place_id,
-                    location: place.geometry.location
-                }));
-                marker.setVisible(true);
-            });
-        }
 
         function goToLogin() {
             UserService.setLoginRedirect("/search");
